0784553be3e9bf77127ceac1b1034b91c767913f
GumemuraPCdGU <gumemura123@gmail.com>
2020-09-06 04:00:06 -0300

    enter click and product anem fidner

--- amazon_scapper.py
+++ amazon_scapper.py
@@ -1,4 +1,14 @@
+import time 
 from selenium import webdriver
+
+'''
+# Importar a classe WebDriverWait
+from selenium.webdriver.support.ui import WebDriverWait
+# Importar a classe que contém as funções e aplicar um alias
+from selenium.webdriver.support import expected_conditions as EC
+# Importar classe para ajudar a localizar os elementos
+from selenium.webdriver.common.by import By
+'''
 
 url = 'https://www.amazon.com.br/'
 
@@ -7,19 +17,46 @@ class amazon_scraper:
         self.url = url
         self.driver = webdriver.Firefox()
         self.search_bar = 'twotabsearchtextbox'     # search box id
-        self.button_search = 'nav-search-submit-text'  # search button id
-        self.product_div_class = 'a-size-base-plus a-color-base a-text-normal' #product name div class
+        self.button_search = 'nav-input' # search button class
+        self.product_div_class = 'a-size-base-plus' #product name div class
     
     def enter_site(self):
+        '''
+        Opens the browser on url page
+        '''
         self.driver.get(self.url)
     
     def search_and_enter(self, word = 'None'):
+        '''
+        Type desired word in search box then hits search
+        
+        :Args:
+        word -> the word to be searched
+        '''
         self.driver.find_element_by_id(self.search_bar).send_keys(word)
-        self.driver.find_element_by_id(self.button_search).click()
+        for elem in self.driver.find_elements_by_class_name(self.button_search):
+            if elem.get_attribute("type") == "submit":
+                elem.click()
+                break
     
     def grab_product(self):
-        self.driver.find_elements_by_class_name(self.product_div_class)
-    
+        '''
+        
+        '''
+        for elem in self.driver.find_elements_by_class_name(self.product_div_class):
+            print(elem.text)
+
+        
+    def time_break(self, break_time):
+        '''
+        Gives the execution a break
+        :Args:
+        time -> amount of seconds the break will last
+        '''
+        time.sleep(break_time)
+
 a = amazon_scraper(url)
 a.enter_site()
 a.search_and_enter('banana')
+a.time_break(2)
+a.grab_product()
